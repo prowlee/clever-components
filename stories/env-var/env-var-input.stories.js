453e483489a5cd3bd228ff707769428e3763065a
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-04-25 19:49:52 +0200

    --wip--

--- stories/env-var/env-var-input.stories.js
+++ stories/env-var/env-var-input.stories.js
@@ -6,20 +6,17 @@ import { withCustomEventActions } from '../lib/event-action';
 const eventNames = ['env-var-input:input', 'env-var-input:delete', 'env-var-input:keep'];
 
 storiesOf('env-var/<env-var-input>', module)
-  .add('all', () => withCustomEventActions(...eventNames)(() => `
-    
-    <env-var-input name="EMPTY" value=""></env-var-input>
-    
-    <env-var-input name="PRISTINE" value="pristine value"></env-var-input>
-    
-    <env-var-input name="NEW" value="new value" is-new></env-var-input>
-    
-    <env-var-input name="NEW_EDITED" value="new deleted value" is-new is-edited></env-var-input>
-    
-    <env-var-input name="EDITED" value="edited value" is-edited></env-var-input>
-    
-    <env-var-input name="DELETED" value="deleted value" is-deleted></env-var-input>
-    
-    <env-var-input name="EDITED_DELETED" value="edited deleted value" is-edited is-deleted></env-var-input>
-    
-  `), { notes });
+  .add('all', () => withCustomEventActions(...eventNames)(() => {
+    // language=HTML
+    return `
+      <env-var-input name="EMPTY" value=""></env-var-input>
+      <env-var-input name="PRISTINE" value="pristine value"></env-var-input>
+      <env-var-input name="NEW" value="new value" is-new></env-var-input>
+      <env-var-input name="NEW_EDITED" value="new deleted value" is-new is-edited></env-var-input>
+      <env-var-input name="EDITED" value="edited value" is-edited></env-var-input>
+      <env-var-input name="DELETED" value="deleted value" is-deleted></env-var-input>
+      <env-var-input name="EDITED_DELETED" value="edited deleted value" is-edited is-deleted></env-var-input>
+      <env-var-input name="VERY_LONG_NAME_THAT_IS_ACTUALLY_TOO_LONG_TOO_DISPLAY_OMG_WHAT_IS_HAPPENING" value="value for long name"></env-var-input>
+      <env-var-input name="LONG_VALUE" value="very long name that is actually too long too display omg what is happening"></env-var-input>
+    `;
+  }), { notes });
